fc7546f117cad791cf0d71f5d7aa668acaf5fcfe
Amos <wky1991@gmail.com>
2015-05-30 13:25:03 +0800

    [*] change gemfile name to Gemfile and move method before defining it

--- feed.rb
+++ feed.rb
@@ -2,6 +2,13 @@ require 'rss'
 require 'json'
 require 'rest-client'
 
+def send_request request
+  response = RestClient.post( 
+    'https://leancloud.cn/1.1/classes/insights', 
+    request,
+    :content_type => "application/json", :'x-avoscloud-request-sign' => "2c12a09cca51ff10ef42c190dd943bc6,1432918335", :'X-AVOSCloud-Application-Id' => '0erqjpcv2cbkk24t75wrb7l4n11avy0u1xl8fmkxi11nhlr9')
+end
+
 rss = RSS::Parser.parse('http://insights.thoughtworkers.org/feed/', false)
 rss.items.each do |item|
   hash = {}
@@ -11,11 +18,4 @@ rss.items.each do |item|
   hash['content_encoded'] = item.content_encoded
 
   send_request hash.to_json
-end
-
-def send_request request
-  response = RestClient.post( 
-    'https://leancloud.cn/1.1/classes/insights', 
-    request,
-    :content_type => "application/json", :'x-avoscloud-request-sign' => "2c12a09cca51ff10ef42c190dd943bc6,1432918335", :'X-AVOSCloud-Application-Id' => '0erqjpcv2cbkk24t75wrb7l4n11avy0u1xl8fmkxi11nhlr9')
 end
