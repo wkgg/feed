6031f519305c43871a2152f1869f62a1bcfa2e65
Amos <wky1991@gmail.com>
2015-05-30 16:33:28 +0800

    using leanCloud batch send requests

--- feed.rb
+++ feed.rb
@@ -2,14 +2,30 @@ require 'rss'
 require 'json'
 require 'rest-client'
 
-def send_request request
+def batch_send_requests requests
   response = RestClient.post( 
-    'https://leancloud.cn/1.1/classes/insights', 
-    request,
+    'https://api.leancloud.cn/1.1/batch', 
+    requests,
     :content_type => "application/json", :'x-avoscloud-request-sign' => "2c12a09cca51ff10ef42c190dd943bc6,1432918335", :'X-AVOSCloud-Application-Id' => '0erqjpcv2cbkk24t75wrb7l4n11avy0u1xl8fmkxi11nhlr9')
 end
 
+def build_requests insights
+  data = []
+  insights.each do|insight|
+    request = {}
+    request["method"] = "POST"
+    request["path"] = "/1.1/classes/insights"
+    request["body"] = insight
+
+    data.push request
+  end
+  requests = {}
+  requests["requests"] = data
+  requests.to_json
+end
+
 rss = RSS::Parser.parse('http://insights.thoughtworkers.org/feed/', false)
+insights = []
 rss.items.each do |item|
   hash = {}
   hash['title'] = item.title
@@ -17,5 +33,8 @@ rss.items.each do |item|
   hash['putDate'] = item.pubDate
   hash['content_encoded'] = item.content_encoded
 
-  send_request hash.to_json
+  insights.push hash
 end
+
+requests = build_requests insights
+batch_send_requests requests
